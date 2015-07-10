7325f7624080c6b9f869905b55a4b011dc4c36ca
iEdu48 <edugarfol@gmail.com>
2015-07-10 03:03:31 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -36,7 +36,6 @@ exports.Quiz = Quiz; // exportar definicion de la tabla Quiz
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
     // then(..) ejecuta el manejador una vez creada la tabla
-    Quiz.destroy({});
     Quiz.count().then(function (count){
         if(count === 0) {   // la tabla se inicializa solo si está vacía
             Quiz.bulkCreate(
